992e08cceeb1983d426865bade410eb84e149615
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-02-27 20:03:27 +0800

    Implement prepared statements for database insertion and rearranged generation order due to FK constaint

--- faker.php
+++ faker.php
@@ -16,8 +16,9 @@ if ($connection->connect_error) {
 // Initialize Faker
 $faker = Faker\Factory::create('en_PH');
 
-// Generate Employee Data [200]
-for ($i = 0; $i < 200; $i++){
+// Generate Office Data [50]            NEEDS TO BE FIRST BECAUSE OF FK CONSTRAINT
+$stmt = $connection->prepare("INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
+for ($i = 0; $i < 50; $i++) {
     $name = $faker->company;
     $contactnum = $faker->phoneNumber;
     $email = $faker->companyEmail;
@@ -26,19 +27,30 @@ for ($i = 0; $i < 200; $i++){
     $country = 'Philippines';
     $postal = $faker->postcode;
 
-    $sql = "INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES ('$name', '$contactnum', '$email', '$address', '$city', '$country', '$postal')";
-    $connection->query($sql);
+    $stmt->bind_param("sssssss", $name, $contactnum, $email, $address, $city, $country, $postal);
+    $stmt->execute();
 }
+echo "Office Data Generated!\n";
 
-// Generate Office Data [50]
-for ($i = 0; $i < 50; $i++){
-    // Code here
-}
+// Generate Employee Data [200]
+// for ($i = 0; $i < 200; $i++){
+//     $name = $faker->company;
+//     $contactnum = $faker->phoneNumber;
+//     $email = $faker->companyEmail;
+//     $address = $faker->address;
+//     $city = $faker->city;
+//     $country = 'Philippines';
+//     $postal = $faker->postcode;
+
+//     $sql = "INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES ('$name', '$contactnum', '$email', '$address', '$city', '$country', '$postal')";
+//     $connection->query($sql);
+// }
+// echo "Employee Data Generated!\n";
+
+
 
 // Generate Transaction Data [500]
 for ($i = 0; $i < 500; $i++){
     // Code here
 }
 ?>
-
-
