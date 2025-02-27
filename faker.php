89f9d3484e5f94ecda00b93f43e6f898cfe77d3f
Diane Nicole R. Antonio <202280159@psu.palawan.edu.ph>
2025-02-27 20:19:34 +0800

    Added employee data generation with prepared statements

--- faker.php
+++ faker.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
 
+
 // Database Connection
 $host = 'localhost';
 $db = 'fakerdb';
@@ -13,11 +14,13 @@ if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
+
 // Initialize Faker
 $faker = Faker\Factory::create('en_PH');
 
+
 // Generate Office Data [50]            NEEDS TO BE FIRST BECAUSE OF FK CONSTRAINT
-$stmt = $connection->prepare("INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
+$stmt1 = $connection->prepare("INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
 for ($i = 0; $i < 50; $i++) {
     $name = $faker->company;
     $contactnum = $faker->phoneNumber;
@@ -27,26 +30,24 @@ for ($i = 0; $i < 50; $i++) {
     $country = 'Philippines';
     $postal = $faker->postcode;
 
-    $stmt->bind_param("sssssss", $name, $contactnum, $email, $address, $city, $country, $postal);
-    $stmt->execute();
+    $stmt1->bind_param("sssssss", $name, $contactnum, $email, $address, $city, $country, $postal);
+    $stmt1->execute();
 }
-echo "Office Data Generated!\n";
+echo "Office Data Generated!<br>";
+
 
 // Generate Employee Data [200]
-// for ($i = 0; $i < 200; $i++){
-//     $name = $faker->company;
-//     $contactnum = $faker->phoneNumber;
-//     $email = $faker->companyEmail;
-//     $address = $faker->address;
-//     $city = $faker->city;
-//     $country = 'Philippines';
-//     $postal = $faker->postcode;
+$stmt2 = $connection->prepare("INSERT INTO Employee (lastname, firstname, office_id, address) VALUES (?, ?, ?, ?)");
+for ($i = 0; $i < 200; $i++) {
+    $lastname = $faker->lastName;
+    $firstname = $faker->firstName;
+    $office_id = $faker->numberBetween(1, 50); // FK
+    $address = $faker->address;
 
-//     $sql = "INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES ('$name', '$contactnum', '$email', '$address', '$city', '$country', '$postal')";
-//     $connection->query($sql);
-// }
-// echo "Employee Data Generated!\n";
-
+    $stmt2->bind_param("ssis", $lastname, $firstname, $office_id, $address);
+    $stmt2->execute();
+}
+echo "Employee Data Generated!\n";
 
 
 // Generate Transaction Data [500]
