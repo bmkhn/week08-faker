1fbda6f6c227ff299cb5924a2d3d2da78ecb9328
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-03-11 11:20:29 +0800

    Add lab2.php

---
+++ lab2.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+$genres = ['Fiction', 'Mystery', 'Science Fiction', 'Fantasy', 'Romance', 'Thriller', 'Historical', 'Horror'];
+
+$books = [];
+for ($i = 0; $i < 15; $i++) {
+    $books[] = [
+        'title' => $faker->words(3, true),
+        'author' => $faker->name,
+        'genre' => $genres[array_rand($genres)],
+        'publication_year' => $faker->dateTimeBetween('-2 years', 'now')->format('Y'),
+        'isbn' => $faker->isbn13,
+        'summary' => $faker->words(10, true),
+    ];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fake Book Records</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Fake Book Records</h1>
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Genre</th>
+                    <th>Publication Year</th>
+                    <th>ISBN</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($books as $book): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($book['title']); ?></td>
+                        <td><?php echo htmlspecialchars($book['author']); ?></td>
+                        <td><?php echo htmlspecialchars($book['genre']); ?></td>
+                        <td><?php echo htmlspecialchars($book['publication_year']); ?></td>
+                        <td><?php echo htmlspecialchars($book['isbn']); ?></td>
+                        <td><?php echo htmlspecialchars($book['summary']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
